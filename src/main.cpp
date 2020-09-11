d65ed96c6101b5abf4c8edfc2db2c5679a7a4f11
Julia Reid <jureid@microsoft.com>
2020-09-10 23:29:33 -0700

    working fib program

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <fmt/format.h>
+#include <cppitertools/itertools.hpp>
+
+/**
+ * @brief sample fibonacci program
+ * TEST
+ * 
+ * @param n 
+ * @return int 
+ */
+int fib(int n) {
+	int a = 0, b = 1;
+
+	for ([[maybe_unused]] int i : iter::range(n)) {
+		auto tmp = b;
+		b = a + b;
+		a = tmp;
+	}
+
+	return a;
+}
+
+int main() {
+	for (int i : iter::range(15)) {
+		fmt::print("fib({}) = {}\n", i, fib(i));
+	}
+}
